e516ed2457aa155107d46876f539835b7f896153
tomaslobato <tlobatodev@gmail.com>
2024-08-02 19:44:13 -0300

    global .env

--- api/index.go
+++ api/index.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/tomaslobato/mailist/router"
 )
 
@@ -14,16 +13,10 @@ var db *sql.DB
 var appPwd string
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to load .env.local file")
-		os.Exit(1)
-	}
-
 	dbUrl := os.Getenv("TURSO_URL")
 	appPwd = os.Getenv("GMAIL_APP_PASSWORD")
 
-	db, err = sql.Open("libsql", dbUrl)
+	db, err := sql.Open("libsql", dbUrl)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to open db: %s", err)
 		os.Exit(1)
