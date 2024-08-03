36c90d2b423c78941cc520a9938f4708d2de9392
tomaslobato <tlobatodev@gmail.com>
2024-08-03 17:53:03 -0300

    /api routes, added simple auth to /api/send-email

--- router/router.go
+++ router/router.go
@@ -8,10 +8,10 @@ import (
 func SetupRoutes(db *sql.DB, appPwd, adminCode string) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /emails", func(w http.ResponseWriter, r *http.Request) { GetEmails(w, db) })
-	mux.HandleFunc("GET /emails/{id}", func(w http.ResponseWriter, r *http.Request) { GetEmailById(w, r, db) })
-	mux.HandleFunc("POST /emails", func(w http.ResponseWriter, r *http.Request) { AddEmail(w, r, db) })
-	mux.HandleFunc("POST /emails/send", func(w http.ResponseWriter, r *http.Request) { SendEmail(w, r, db, appPwd, adminCode) })
+	mux.HandleFunc("GET /api/email", func(w http.ResponseWriter, r *http.Request) { GetEmails(w, db) })
+	mux.HandleFunc("GET /api/email/{id}", func(w http.ResponseWriter, r *http.Request) { GetEmailById(w, r, db) })
+	mux.HandleFunc("POST /api/email", func(w http.ResponseWriter, r *http.Request) { AddEmail(w, r, db) })
+	mux.HandleFunc("POST /api/send-email", func(w http.ResponseWriter, r *http.Request) { SendEmail(w, r, db, appPwd, adminCode) })
 
 	return mux
 }
